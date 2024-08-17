db59d2ccb82578cd6e9316187dd149c8a8480e80
DocDBrown <dr.dbrown@icloud.com>
2024-08-17 15:48:06 +1000

    Listening to Pete Murray

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@
 
 fn main() {
     println!("Fuck you Linux, Windows and MacOS!");
-    println!("The boxning Knagroo is coming to teach you a lesson!")
+    println!("The boxing Kangaroo is coming to teach you a lesson!")
     println!("Project in early stages still...")
 }
