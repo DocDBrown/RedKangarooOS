409ae19ad74af67f690cf1ee6d78845272be4c99
DocDBrown <dr.dbrown@icloud.com>
2024-08-17 15:40:24 +1000

    You have to start somewhere

---
+++ RedKangaroo/src/main.rs
@@ -0,0 +1,8 @@
+* Copyright © [2024] [Nox Ignis].
+* This file is part of [Red Kangaroo OS Project].
+
+fn main() {
+    println!("Fuck you Linux, Windows and MacOS!");
+    println!("The boxning Knagroo is coming to teach you a lesson!")
+    println!("Project in early stages still...")
+}
