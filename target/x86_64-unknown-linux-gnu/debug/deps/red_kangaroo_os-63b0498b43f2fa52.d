eb7819ba7a26ada8ffb4d158a42b6a24eb1ba26d
DocDBrown <dr.dbrown@icloud.com>
2024-08-19 01:15:46 +1000

    ...

---
+++ target/x86_64-unknown-linux-gnu/debug/deps/red_kangaroo_os-63b0498b43f2fa52.d
@@ -0,0 +1,5 @@
+/Users/duncanbrown/Desktop/RustProjects/RedKangarooOS/target/x86_64-unknown-linux-gnu/debug/deps/libred_kangaroo_os-63b0498b43f2fa52.rmeta: src/main.rs
+
+/Users/duncanbrown/Desktop/RustProjects/RedKangarooOS/target/x86_64-unknown-linux-gnu/debug/deps/red_kangaroo_os-63b0498b43f2fa52.d: src/main.rs
+
+src/main.rs:
---
+++ target/x86_64-unknown-linux-gnu/debug/deps/red_kangaroo_os-c80ba3d25ea8b023.d
@@ -0,0 +1,5 @@
+/Users/duncanbrown/Desktop/RustProjects/RedKangarooOS/target/x86_64-unknown-linux-gnu/debug/deps/libred_kangaroo_os-c80ba3d25ea8b023.rmeta: src/main.rs
+
+/Users/duncanbrown/Desktop/RustProjects/RedKangarooOS/target/x86_64-unknown-linux-gnu/debug/deps/red_kangaroo_os-c80ba3d25ea8b023.d: src/main.rs
+
+src/main.rs:
